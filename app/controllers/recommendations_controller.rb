47d4b3de7448ec0a580721935974d03a61f86cdd
amingilani <amin@gilani.me>
2017-07-14 04:54:18 +0500

    beautify the controller

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,8 +1,8 @@
 class RecommendationsController < ApplicationController
   def new
     @recommendation = Recommendation.new(
-    body: File.read(Rails.root + 'lib/template-letter.txt'),
-    address_country: 'US'
+      body: File.read(Rails.root + 'lib/template-letter.txt'),
+      address_country: 'US'
     )
   end
 end
