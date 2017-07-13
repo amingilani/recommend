b73b4870411f376c1287bbec30373018d7e470dd
amingilani <amin@gilani.me>
2017-07-14 04:42:26 +0500

    Add recommendation letter fields

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,4 +1,8 @@
 class RecommendationsController < ApplicationController
   def new
+    @recommendation = Recommendation.new(
+    body: File.read(Rails.root + 'lib/template-letter.txt'),
+    address_country: 'US'
+    )
   end
 end
