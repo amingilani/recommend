77bb46a3fdeb1f20e158f67ab170ff3d85af07da
amingilani <amin@gilani.me>
2017-07-15 00:37:48 +0500

    Install model annotations

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
 gem 'country_select'
+gem 'annotate'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,2 +1,34 @@
+# <!-- BEGIN ANNOTATION -->
+# ## Schema Information
+#
+# Table name: `recommendations`
+#
+# ### Columns
+#
+# Name                    | Type               | Attributes
+# ----------------------- | ------------------ | ---------------------------
+# **`id`**                | `integer`          | `not null, primary key`
+# **`name`**              | `string`           |
+# **`email`**             | `string`           |
+# **`phone_number`**      | `string`           |
+# **`phone_extension`**   | `string`           |
+# **`fax_number`**        | `string`           |
+# **`organization`**      | `string`           |
+# **`position`**          | `string`           |
+# **`subject`**           | `string`           |
+# **`body`**              | `text`             |
+# **`signature_id`**      | `string`           |
+# **`signature_status`**  | `integer`          |
+# **`address_line_1`**    | `string`           |
+# **`address_line_2`**    | `string`           |
+# **`address_city`**      | `string`           |
+# **`address_state`**     | `string`           |
+# **`address_zip`**       | `string`           |
+# **`address_country`**   | `string`           |
+# **`created_at`**        | `datetime`         | `not null`
+# **`updated_at`**        | `datetime`         | `not null`
+#
+# <!-- END ANNOTATION -->
+
 class Recommendation < ApplicationRecord
 end
