b24bd1b859057915a887bd0581f7e48427d1b1d1
amingilani <amin@gilani.me>
2017-07-16 23:17:24 +0500

    Update signature_id when sent out

--- app/services/get_accept_service.rb
+++ app/services/get_accept_service.rb
@@ -9,23 +9,24 @@ class GetAcceptService
   def send_for_signature(r)
     return puts 'sent to sign for #{r.first_name}' unless Rails.env.production?
     begin
-    @api.document.create('name':                 'Recommending Amin Gilani',
-                         'external_id':          r.slug,
-                         'file_url':             "https://recommend.gilani.me#{Rails.application.routes.url_helpers.recommendation_path(r, format: :pdf)}",
-                         'type':                 'other',
-                         'recipients':           [{
-                           'email':      r.email,
-                           'first_name': r.first_name,
-                           'last_name':  r.last_name,
-                           'role':       'signer',
-                           'order_num':  0
-                         }],
-                         'is_signing':           true,
-                         'is_signing_biometric': true,
-                         'is_reminder_sending':  true,
-                         'is_signing_forward':   false,
-                         'is_sms_sending':       false,
-                         'is_automatic_sending': true)
+      res = @api.document.create('name':                 'Recommending Amin Gilani',
+                                 'external_id':          r.slug,
+                                 'file_url':             "https://recommend.gilani.me#{Rails.application.routes.url_helpers.recommendation_path(r, format: :pdf)}",
+                                 'type':                 'other',
+                                 'recipients':           [{
+                                   'email':      r.email,
+                                   'first_name': r.first_name,
+                                   'last_name':  r.last_name,
+                                   'role':       'signer',
+                                   'order_num':  0
+                                 }],
+                                 'is_signing':           true,
+                                 'is_signing_biometric': true,
+                                 'is_reminder_sending':  true,
+                                 'is_signing_forward':   false,
+                                 'is_sms_sending':       false,
+                                 'is_automatic_sending': true)
+      r.update(signature_id: res['id'])
     rescue GetAccept::RequestError
       set_api
     end
