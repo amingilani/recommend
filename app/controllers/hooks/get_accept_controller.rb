470c079964380c17d936704a4da600da39d3a38a
amingilani <amin@gilani.me>
2017-07-17 00:55:59 +0500

    disable CSRF checks on notifications

--- app/controllers/hooks/get_accept_controller.rb
+++ app/controllers/hooks/get_accept_controller.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 class Hooks::GetAcceptController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def notification
-    event = request.body.read
-    r = Recommendation.find_by slug: event['document']['external_id']
-    r.update(signature_date: event['document']['external_id'])
+    r = Recommendation.find_by slug: params['document']['external_id']
+    r.update(signature_date: params['document']['sign_date'])
+    render json: {success: true, message: 'Okay!'}
   end
 end
