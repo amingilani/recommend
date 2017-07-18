e9b666728a9b3dbeb4539f485027967e96d8fea9
amingilani <amin@gilani.me>
2017-07-18 05:08:21 +0500

    Import the HTML5 Story Up template as the home page

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,5 @@
+//= require jquery/dist/jquery.min.js
+//= require jquery.scrollex/jquery.scrollex.min.js
+//= require jquery.scrolly/jquery.scrolly.js
+//= require skel-framework-npm/dist/skel.min.js
+//= require_tree ./home
