3c1309dea89111cad8df276122f6de5d3af62155
amingilani <amin@gilani.me>
2017-07-18 09:00:42 +0500

    Add override for special buttons

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,4 +2,5 @@
 //= require jquery.scrollex/jquery.scrollex.min.js
 //= require jquery.scrolly/jquery.scrolly.js
 //= require skel-framework-npm/dist/skel.min.js
+//= require semantic-ui/dist/semantic.min.js
 //= require_tree ./home
